1b5de8bfb95dc4e340ca0041a9737ee9682202ed
JuanPas12 <juanptafolla@gmail.com>
2023-04-17 12:38:20 -0600

    Ejecución y comentarios

---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,11 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+Esta es la parte intermedia del proyecto
+En esta parte realizamos los chequeos
+Por ultimo vamos a la ultima linea
+*/
+
+//Este es el mensaje de despedida
+console.log("Adiós");
