0362568f8292cf8e3bc3fd73bc1325c05acb57de
JuanPas12 <juanptafolla@gmail.com>
2023-04-18 12:52:30 -0600

    Notación en JavaScript

---
+++ mi-primer-proyecto-de-node/notacion.js
@@ -0,0 +1,26 @@
+// Notación
+// ; --> Delimitar el final de una linea
+const b = 4;
+b + 4;
+//Al dia de hoy no pasa nada si no se usa el punto y coma, pero lo mejor para prevenir errores es utilizarlo
+
+// . --> Se utiliza en los objetos para acceder a los atributos
+//movil.ancura
+
+// [] --> listas, arreglos o array
+const ar = [1, 2, 3, 4];
+console.log(ar[2]);
+
+// () --> Funciones
+function suma(a, b) {
+    //se escribe la función
+}
+// {} --> Objetos, funciones y estructuras de control
+const movil = {
+    ancura: 5,
+    altura: 10,
+};
+
+if (true) {
+    //todo lo que haya entre llaves
+}
