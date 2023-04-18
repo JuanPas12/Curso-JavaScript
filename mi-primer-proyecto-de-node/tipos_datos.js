9b89dc2c55a944c83b2e03b960a32937e51eeac6
JuanPas12 <juanptafolla@gmail.com>
2023-04-18 12:32:44 -0600

    Tipos de datos

---
+++ mi-primer-proyecto-de-node/tipos_datos.js
@@ -0,0 +1,24 @@
+//Tipos primitivos
+
+//number
+4;
+0;
+
+//string
+"Hola mundo";
+'Hola mundo';
+
+//booleanos
+true;
+false;
+
+//nulo y undefined
+null;
+undefined;
+
+//null, undefined, false, 0 ==> Todas son Falsy
+if (null) {
+    console.log("cumple");
+} else{
+    console.log("no cumple");
+}
