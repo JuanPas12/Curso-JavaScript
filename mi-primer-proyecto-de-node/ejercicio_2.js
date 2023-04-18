af269edb52f0fc8a7c09000c2359ddfa01f34900
JuanPas12 <juanptafolla@gmail.com>
2023-04-18 13:19:36 -0600

    Ejercicio

---
+++ mi-primer-proyecto-de-node/ejercicio_2.js
@@ -0,0 +1,13 @@
+let nombre = "Juan Pablo";
+let edad = 22;
+let developer = true;
+let fecha_nacimiento = new Date("July 12 2000");
+const libro_favorito = {
+    titulo: "Las 48 leyes del poder",
+    autor: "Robert Grrene",
+    fecha: "1998",
+    url: "https://www.amazon.com.mx/Leyes-Del-Poder-Laws-Power/dp/9500826038",
+};
+
+const lista = [nombre, edad, developer, fecha_nacimiento, libro_favorito];
+console.log(lista);
