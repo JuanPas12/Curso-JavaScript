ffb834bc9b36c90c56de9b137fe28218fe436c5c
JuanPas12 <juanptafolla@gmail.com>
2023-04-19 11:44:58 -0600

    Bifurcaciones if else y switch

---
+++ Estructuras de control/if-else-else-if.js
@@ -0,0 +1,17 @@
+// If else + else if
+
+let nota = 5;
+
+if (nota === 5) {
+    console.log("Enhorabuena has conseguido un sobresaliente");
+} else if (nota === 4) {
+    console.log("Buen trabajo pero podria mejorar");
+} else if (nota === 3) {
+    console.log("Has obtenido un suficientge");
+} else if (nota === 2) {
+    console.log("No has aprobado pero por poco");
+} else if (nota === 1) {
+    console.log("No has estudiado nada");
+} else {
+    console.log("Error, introduce una nota entre 1 y 5");
+}
---
+++ Estructuras de control/switch.js
@@ -0,0 +1,24 @@
+// Switch
+
+let nota = 5;
+
+switch (nota) {
+    case 5:
+        console.log("Enhorabuena has obtenido la nota mayor");
+        break;
+    case 4:
+        console.log("Buen trabajo pero podria mejorar");
+        break;
+    case 3:
+        console.log("Has obtenido un suficientge");
+        break;
+    case 2:
+        console.log("No has aprobado pero por poco");
+        break;
+    case 1:
+        console.log("No has estudiado nada");
+        break;
+    default:
+        console.log("Error, introduce una nota entre 1 y 5");
+        break;
+}
