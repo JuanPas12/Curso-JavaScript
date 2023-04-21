fbe202c50974f77a98184dfe8a1c3be09de2b1e3
JuanPas12 <juanptafolla@gmail.com>
2023-04-21 12:15:36 -0600

    Ejercicio

---
+++ Numbers/ejercicio.js
@@ -0,0 +1,20 @@
+let altura_cm = 185;
+console.log(altura_cm);
+
+let altura_m = 1.85;
+console.log(altura_m);
+
+let peso = 86.3;
+console.log(peso);
+
+let altura_m_r = Math.round(altura_m);
+console.log(altura_m_r);
+
+let peso_r = Math.round(peso);
+console.log(peso_r);
+
+let max_value_mas_1 = Number.MAX_VALUE + 1;
+let max_value = Number.MAX_VALUE;
+let iguales = max_value_mas_1 === max_value;
+
+console.log(iguales);
