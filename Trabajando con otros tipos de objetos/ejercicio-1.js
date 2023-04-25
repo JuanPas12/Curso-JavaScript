69f7bdd55d354b1f6530a0dd9ea47fa6e15ec568
JuanPas12 <juanptafolla@gmail.com>
2023-04-25 12:23:36 -0600

    Ejercicio 1

---
+++ Trabajando con otros tipos de objetos/ejercicio-1.js
@@ -0,0 +1,10 @@
+let familia = ["Juan Pablo", "Juan Manuel", "Ma de Lourdes", "Fernanda", "Karina"];
+
+let set = new Set(familia);
+console.log(set);
+
+set.add("Juan Pablo");
+console.log(set);
+
+set.add("Javascript");
+console.log(set);
