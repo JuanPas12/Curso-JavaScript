ae2ec5b18728fce9341d46cc7cc6d9e1cd76330d
JuanPas12 <juanptafolla@gmail.com>
2023-05-04 12:50:20 -0600

    Reglas temporales y scrips para ejecución de ESLint en nuestro código

--- ESLint/eslint-npm/index.js
+++ ESLint/eslint-npm/index.js
@@ -1,4 +1,17 @@
 const nombre = "Juan Pablo"
 
-// En esta linea quiero tener comillas simples o quiero que me desactives las reglas
+/* eslint-disable */
+
 const persona2 = 'Jose'
+
+/* eslint-enable */
+
+// En esta linea quiero tener comillas simples o quiero que me desactives las reglas
+const nuevoString = 'Hola'; // eslint-disable-line
+
+/* eslint-disable-next-line quotes*/
+const string2 = 'Hola'
+
+console.log("hola")
+
+const persona3 = "Ramiro"
